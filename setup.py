5ab7f64669f1b7b215c2c590677ff82197043e0c
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2024-01-22 14:20:50 +0100

    Creation de setup.py
    Création et premier commit du fichier setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{Quiz_Game}',
+ version='1.0',
+ author='{correia_alexandre}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
