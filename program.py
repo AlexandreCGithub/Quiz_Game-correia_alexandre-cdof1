b0d021fbdbf85c371ce6196f6d8d10affbbe0051
JasonL1203 <132206978+JasonL1203@users.noreply.github.com>
2024-01-24 22:28:55 +0100

    New feature (#7)
    * improveREADME
    
    * gestion erreurs commit
    
    * gestion erreurs
    
    * new feature

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 def poser_question(question, reponses, bonne_reponse):
     print(question)
@@ -7,7 +8,7 @@ def poser_question(question, reponses, bonne_reponse):
 
     while True:
         choix_utilisateur = input("Votre réponse (entrez le numéro) : ")
-        if choix_utilisateur.isdigit() and 1 <= int(choix_utilisateur) <= 4:
+        if choix_utilisateur.isdigit() and 1 <= int(choix_utilisateur) <= len(reponses):
             choix_utilisateur = int(choix_utilisateur)
             break
         else:
@@ -25,19 +26,25 @@ def main():
     score = 0
 
     with open(fichier_quiz, "r") as file:
-        lignes = file.readlines()
+        lignes = file.readlines()[:10]  # Read only the first 10 lines
 
-    for ligne in lignes:   
+    num_questions = len(lignes)
+    print(f"Le quiz contient {num_questions} questions.")
+
+    random.shuffle(lignes)  # Shuffle the order of questions
+
+    for ligne in lignes:
         elements = ligne.strip().split(", ")
         question = elements[0]
         reponses = elements[1:-1]
         bonne_reponse = elements[-1]
         if poser_question(question, reponses, bonne_reponse):
-            score = score + 1
-        input("Appuyez sur Entrée pour continuer...")  # Attendez que l'utilisateur appuie sur Entrée
+            score += 1
+        input("Appuyez sur Entrée pour continuer...")  # Wait for user to press Enter
         os.system('cls' if os.name == 'nt' else 'clear')
 
-    print("Terminé. Votre score est de " + str(score) + " sur 10")
+    print("Terminé. Votre score est de " + str(score) + " sur " + str(num_questions))
 
 if __name__ == "__main__":
     main()
+
